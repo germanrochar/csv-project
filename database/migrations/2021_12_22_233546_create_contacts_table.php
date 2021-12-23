e5ccfe06a6f037c166a271e0d3a48a9df357dd4a
German Rocha <german.rocha@adgatemedia.com>
2021-12-22 17:25:18 -0700

    Create migrations to set up the db

---
+++ database/migrations/2021_12_22_233546_create_contacts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContactsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('team_id');
+            $table->string('name')->nullable();
+            $table->string('phone');
+            $table->string('email')->nullable();
+            $table->integer('sticky_phone_number_id')->nullable();
+            $table->timestamps();
+        });
+
+        DB::statement('ALTER TABLE contacts AUTO_INCREMENT = 256;');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contacts');
+    }
+}
---
+++ database/migrations/2021_12_22_233614_create_custom_attributes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomAttributesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('custom_attributes', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('contact_id');
+            $table->string('key');
+            $table->string('value');
+            $table->timestamps();
+        });
+        DB::statement('ALTER TABLE custom_attributes AUTO_INCREMENT = 256;');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('custom_attributes');
+    }
+}
