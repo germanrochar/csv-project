191afdaa3181f8b04c97f90c623ab6c0774c9fde
German Rocha <german.rocha@adgatemedia.com>
2021-12-24 09:48:00 -0700

    Create form request and rule object to validate mappings request

---
+++ app/Http/Requests/ImportContactsRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Rules\EncodedJsonValuesMustBeStrings;
+use App\Rules\EncodedJsonValuesNotEmpty;
+use App\Rules\ValidMapping;
+use Illuminate\Foundation\Http\FormRequest;
+
+class ImportContactsRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true; // Allow all users for the moment
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'csv_file' => 'required|mimes:csv,txt|max:10240',
+            'mapping_keys' => ['required', 'string', new EncodedJsonValuesMustBeStrings, new EncodedJsonValuesNotEmpty], // TODO: add rule
+            'mapping_values' => ['required', 'string', new EncodedJsonValuesMustBeStrings, new EncodedJsonValuesNotEmpty]
+        ];
+    }
+}
