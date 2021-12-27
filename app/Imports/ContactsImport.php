c1ac826b3baa4261ef9ece9decd05211d9aa686f
German Rocha <german.rocha@adgatemedia.com>
2021-12-26 18:01:47 -0700

    Use to collection concern instead of toModel to import contacts

--- app/Imports/ContactsImport.php
+++ app/Imports/ContactsImport.php
@@ -4,12 +4,11 @@ namespace App\Imports;
 
 use App\Mappings;
 use App\Models\Contact;
-use Maatwebsite\Excel\Concerns\ToModel;
-use Maatwebsite\Excel\Concerns\WithBatchInserts;
-use Maatwebsite\Excel\Concerns\WithChunkReading;
+use Illuminate\Support\Collection;
+use Maatwebsite\Excel\Concerns\ToCollection;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
 
-class ContactsImport implements ToModel, WithHeadingRow, WithBatchInserts, WithChunkReading
+class ContactsImport implements ToCollection, WithHeadingRow
 {
     /**
     * @var Mappings Keys: contact columns | Values: csv fields
@@ -27,59 +26,36 @@ class ContactsImport implements ToModel, WithHeadingRow, WithBatchInserts, WithC
         $this->customMappings = $customMappings;
     }
 
-
     /**
-    * @param array $row
-    *
-    * @return \Illuminate\Database\Eloquent\Model|null
-    */
-    public function model(array $row)
+     * ToCollection concern is used instead of ToModel because we are storing two different models here.
+     * If ToModel was used, the logic to create custom attributes will break the batch insert functionality.
+     */
+    public function collection(Collection $rows)
     {
-        // Sanitize data
-        $row = array_map(static function ($item) {
-            return rtrim(addslashes($item));
-        }, $row);
+        foreach ($rows as $row)
+        {
+            // Sanitize data
+            $row = $row->map(function ($item) {
+                return rtrim(addslashes($item));
+            });
 
-        $contact = new Contact([
-            'team_id' => $row[$this->contactMappings->get('team_id')],
-            'phone' => $row[$this->contactMappings->get('phone')],
-            'name' => $this->contactMappings->has('name')
-                ? $row[$this->contactMappings->get('name')]
-                : null,
-            'email' => $this->contactMappings->has('email')
-                ? filter_var($row[$this->contactMappings->get('email')], FILTER_SANITIZE_EMAIL)
-                : null,
-            'sticky_phone_number_id' => $this->contactMappings->has('sticky_phone_number_id')
-                ? $row[$this->contactMappings->get('sticky_phone_number_id')]
-                : null
-        ]);
+            $contact = Contact::create([
+                'team_id' => $row[$this->contactMappings->get('team_id')],
+                'phone' => $row[$this->contactMappings->get('phone')],
+                'name' => $this->contactMappings->has('name')
+                    ? $row[$this->contactMappings->get('name')]
+                    : null,
+                'email' => $this->contactMappings->has('email')
+                    ? filter_var($row[$this->contactMappings->get('email')], FILTER_SANITIZE_EMAIL)
+                    : null,
+                'sticky_phone_number_id' => $this->contactMappings->has('sticky_phone_number_id')
+                    ? $row[$this->contactMappings->get('sticky_phone_number_id')]
+                    : null
+            ]);
 
-        foreach ($this->customMappings->getAll() as $key => $value) {
-            $contact->addCustomAttribute($key, $row[$value]);
+            foreach ($this->customMappings->getAll() as $key => $value) {
+                $contact->addCustomAttribute($key, $row[$value]);
+            }
         }
-
-        return $contact;
-    }
-
-    /**
-     * Set amount of records that will be inserted per batch
-     * to reduce the import duration
-     *
-     * @return int
-    */
-    public function batchSize(): int
-    {
-        return 1000;
-    }
-
-    /**
-     * Set amount of records that will be read from file per batch
-     * to mitigate the increase of memory usage
-     *
-     * @return int
-    */
-    public function chunkSize(): int
-    {
-        return 1000;
     }
 }
